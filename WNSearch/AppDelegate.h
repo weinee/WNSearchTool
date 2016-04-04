99bdb81877fc466faa6821bb94e3baaa73035f13
LiJF_weinee <LiJF_wn@163.com>
2016-04-04 14:53:43 +0800

    Initial Commit

---
+++ WNSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WNSearch
+//
+//  Created by weinee on 16/4/4.
+//  Copyright © 2016年 weinee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WNSearchTests/WNSearchTests.m
@@ -0,0 +1,39 @@
+//
+//  WNSearchTests.m
+//  WNSearchTests
+//
+//  Created by weinee on 16/4/4.
+//  Copyright © 2016年 weinee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WNSearchTests : XCTestCase
+
+@end
+
+@implementation WNSearchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
